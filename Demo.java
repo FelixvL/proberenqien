616d6f207ce9221ffe1e34cdcb23996f3db4e7dd
FelixvL <digitaleservice@gmail.com>
2020-06-15 13:26:43 +0200

    here we go

--- Demo.java
+++ Demo.java
@@ -1,7 +1,11 @@
-class Demo{
-	public static void main(String args[]){
-		System.out.println("hoi allemaal hier");
-		System.out.println("hoi allemaal h33ier");
-	}
-}
+class Demo {
+    public static void main(String[] args){   
+
+    	System.out.println("hoi");
+    }
+} 
+
+
+
+
 
